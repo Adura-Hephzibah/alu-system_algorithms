0e70b16db84d5cc3d48d09b6379b51c1ba6caab5
Adura-Hephzibah <adurahephzibah@gmail.com>
2024-02-13 12:14:33 +0200

    task 8

---
+++ huffman_coding/huffman_tree.c
@@ -0,0 +1,27 @@
+#include "huffman.h"
+#include "heap.h"
+
+/**
+ * huffman_tree - function that builds the Huffman tree
+ * @data: array of characters of size size
+ * @freq: array containing the associated frequencies (of size size too)
+ * @size: size of queue
+ * Return: pointer to the root node of the Huffman tree, or NULL if it fails
+ */
+binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
+{
+	heap_t *priority_queue = NULL;
+	binary_tree_node_t *root_node = NULL;
+
+	priority_queue = huffman_priority_queue(data, freq, size);
+
+	if (!priority_queue)
+		return (NULL);
+
+	while (priority_queue->size > 1)
+		huffman_extract_and_insert(priority_queue);
+
+	root_node = heap_extract(priority_queue);
+	heap_delete(priority_queue, NULL);
+	return (root_node);
+}
